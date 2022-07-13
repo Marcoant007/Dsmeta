680cb0a07106730582521aae5a407e5eaff758fa
Marcoant007 <marcoantnovo@gmail.com>
2022-07-12 23:02:32 -0300

    Create others components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,21 @@
+import Header from "./components/Header/Index"
 import NotificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
+
 
 function App() {
   return (
     <>
-    <h1>Olá Mundo!</h1>
-    <NotificationButton />
+    <Header />
+    <main>
+      <section id="sales">
+        <div className="dsmeta-container">
+          <SalesCard />
+        </div>
+      </section>
+    </main>
+
+    
     </>
   )
 }
