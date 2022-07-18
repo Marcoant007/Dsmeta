a3e9ebfffeae48dcc4bcc03ebf74d150e5b12677
Marcoant007 <marcoantnovo@gmail.com>
2022-07-17 22:41:14 -0300

    realizando alterações

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -9,31 +9,37 @@ import './styles.css';
 
 function SalesCard() {
 
-  const max = new Date();
-  const min = new Date(new Date().setDate(new Date().getDate() - 365));
-  const [minDate, setMinDate] = useState(new Date(min));
-  const [maxDate, setMaxDate] = useState(new Date(max));
+  const min = new Date(new Date().setDate(new Date().getDate() - 365))
+  const max = new Date()
+
+  const [minDate, setMinDate] = useState(min)
+  const [maxDate, setMaxDate] = useState(max)
+
   const [sales, setSales] = useState<Sale[]>([]);
 
   useEffect(() => {
 
-    const dataMin = minDate.toISOString().slice(0, 10);
-    const dataMax = maxDate.toISOString().slice(0, 10);
 
-    axios.get(`${BASE_URL}/sales?minDate=${dataMin}&maxDate${dataMax}`).then(response => {
-      setSales(response.data.content);
-    })
-  }, [minDate, maxDate])
+      const dmin = minDate.toISOString().slice(0, 10);
+      const dmax = maxDate.toISOString().slice(0, 10);
 
+
+      axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then(response => {
+          setSales(response.data.content);
+      });
+  }, [minDate, maxDate]);
+
+  
   return (
     <div className="dsmeta-card">
       <h2 className="dsmeta-sales-title">Vendas</h2>
       <div>
         <div className="dsmeta-form-control-container">
-          <DatePicker selected={minDate} onChange={(date: Date) => setMinDate(date)} className="dsmeta-form-control" dateFormat="dd/MM/yyyy" />
+         
+        <p>Data Inicial</p> <DatePicker selected={minDate} onChange={(date: Date) => setMinDate(date)} className="dsmeta-form-control" dateFormat="dd/MM/yyyy" />
         </div>
         <div className="dsmeta-form-control-container">
-          <DatePicker selected={maxDate} onChange={(date: Date) => setMaxDate(date)} className="dsmeta-form-control" dateFormat="dd/MM/yyyy" />
+        <p>Data Final</p> <DatePicker selected={maxDate} onChange={(date: Date) => setMaxDate(date)} className="dsmeta-form-control" dateFormat="dd/MM/yyyy" />
         </div>
       </div>
       <div>
